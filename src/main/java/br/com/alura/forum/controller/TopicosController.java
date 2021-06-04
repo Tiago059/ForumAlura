5f23d4ca0ecee087da1c6a8fc5c0d7557c0af53a
Ítalo Tiago <theitalotiago@gmail.com>
2021-06-03 21:40:43 -0300

    Implementação da Paginação na listagem dos tópicos.

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,13 +1,16 @@
 package br.com.alura.forum.controller;
 
 import java.net.URI;
-import java.util.List;
 import java.util.Optional;
 
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +19,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -38,12 +42,21 @@ public class TopicosController {
 	private CursoRepository cursoRepository;
 	
 	@GetMapping
-	public List<TopicoDto> lista(String nomeCurso) {
+	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, 
+			@PageableDefault(sort = "id", 
+							 direction = Direction.DESC, 
+							 page = 0,
+							 size = 5) Pageable paginacao) {
+		
+		//Pageable --> page=0&size=10&sort=id,desc&sort=dataCriacao,asc
+		
 		if (nomeCurso == null) {
-			return TopicoDto.toTopicoList(topicosRepository.findAll());
+			Page<Topico> topicos = topicosRepository.findAll(paginacao);
+			return TopicoDto.toTopicoList(topicos);
 		}
 		else {
-			return TopicoDto.toTopicoList(topicosRepository.findByCursoNome(nomeCurso));
+			Page<Topico> topicos = topicosRepository.findByCursoNome(nomeCurso, paginacao);
+			return TopicoDto.toTopicoList(topicos);
 		}
 		
 	}
