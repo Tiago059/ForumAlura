75bb1b600ca2461ab1ec531fe9ab596823a53960
Ítalo Tiago <theitalotiago@gmail.com>
2021-06-03 23:30:57 -0300

    Implementação de login e usuário com senha.

--- src/main/java/br/com/alura/forum/model/Usuario.java
+++ src/main/java/br/com/alura/forum/model/Usuario.java
@@ -28,7 +28,7 @@ public class Usuario implements UserDetails {
 	private String senha;
 	
 	@ManyToMany(fetch = FetchType.EAGER)
-	private List<Perfil> perfis = new ArrayList<>();
+	private List<PerfilUsuario> perfis = new ArrayList<>();
 
 	@Override
 	public int hashCode() {
